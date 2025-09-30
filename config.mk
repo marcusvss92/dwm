96159efefe6fb9829c1de1236d71fa22bc89623c
Marcus Vinicius Salvador <119147574+marcusvss92@users.noreply.github.com>
2025-09-30 12:59:11 -0300

    Update config.mk

--- config.mk
+++ config.mk
@@ -20,7 +20,7 @@ FREETYPEINC = /usr/include/freetype2
 # OpenBSD (uncomment)
 #FREETYPEINC = ${X11INC}/freetype2
 #MANPREFIX = ${PREFIX}/man
-+#KVMLIB = -lkvm
+#KVMLIB = -lkvm
 
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC}
